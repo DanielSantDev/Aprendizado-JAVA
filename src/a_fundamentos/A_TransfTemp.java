5ca08c878a9dffdc59e674a850ef98f57c97d681
Daniel Santana <daniel10gja@gmail.com>
2021-09-30 13:34:33 -0300

    Temperatura

---
+++ src/a_fundamentos/A_TransfTemp.java
@@ -0,0 +1,18 @@
+package a_fundamentos;
+
+public class A_TransfTemp {
+	
+	public static void main(String[] args) {
+		//  ( ºF - 32 ) * 5/9 = ºC
+		
+		final double x = 5.0 / 9.0;
+		final double y = 32;
+		
+		double fahrenheit = 86;
+		double celsius = ( fahrenheit - y ) * x;
+		System.out.println("O resultado é " + celsius +" ºC.");
+		
+		
+	}
+
+}
