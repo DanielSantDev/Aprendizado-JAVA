db998c2eb9948236e8e890fe31753200b450ea2b
Daniel Santana <daniel10gja@gmail.com>
2021-10-01 14:10:35 -0300

    Import

---
+++ src/a_fundamentos/D_Import.java
@@ -0,0 +1,24 @@
+package a_fundamentos;
+
+import java.util.Date;
+
+import javax.swing.JButton;
+
+public class D_Import {
+	
+	public static void main(String[] args) {
+		java.lang.String b = "Boa tarde.";
+		java.lang.System.out.println(b);
+		
+		String s = "Bom Dia";
+		System.out.println(s);
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+		 //CTRL + SHIFT + O = APGAR IMPORT Ñ USADO
+		//JButton botao = new JButton();
+				
+	}
+
+}
