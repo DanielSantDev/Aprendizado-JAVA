1e64d3c7ce70899f3ed644636b4ad21177eb16ff
Daniel Santana <daniel10gja@gmail.com>
2021-10-02 12:46:03 -0300

    Wrappers

---
+++ src/a_fundamentos/G_Wrappers.java
@@ -0,0 +1,36 @@
+package a_fundamentos;
+
+import java.util.Scanner;
+
+public class G_Wrappers {
+	
+	public static void main(String[] args) {
+		
+		//Wrappers são a versão objeto dos tipos primitivos
+		
+		Byte b = 100;
+		Short s = 1000;
+		Integer i = 10000; //int
+		Long l = 100000L;
+		
+		System.out.println(b.byteValue());
+		System.out.println(s.toString());
+		System.out.println(i * 3);
+		System.out.println(l / 3);
+		
+		Float f = 123.0F;
+		System.out.println(f);
+		
+		Double d = 1234.5678;
+		System.out.println(d );
+		
+		Boolean bool = Boolean.parseBoolean("true");
+		System.out.println(bool);
+		System.out.println(bool.toString().toUpperCase());
+		
+		Character c = '#';
+		System.out.println(c + "...");
+		
+	}
+
+}
