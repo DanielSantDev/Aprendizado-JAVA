91e70c1e42b8c83cdb37e706ee36f0b7ee4f5d20
Daniel Santana <daniel10gja@gmail.com>
2021-10-09 14:31:28 -0300

    Operadores Lógicos

---
+++ src/b_fundamentos/C_OpLogicos.java
@@ -0,0 +1,43 @@
+package b_fundamentos;
+
+public class C_OpLogicos {
+	
+	public static void main(String[] args) {
+		
+		boolean condicao1 = true;
+		boolean condicao2 = 3 > 7;
+		
+		System.out.println(condicao1 && condicao2);
+		System.out.println(condicao1 && !condicao2);
+		System.out.println(condicao1 || condicao2);
+		System.out.println(condicao1 ^ condicao2);
+		System.out.println(!!condicao1);
+		System.out.println(!condicao1);
+		System.out.println("\n");
+		
+		//Tabela Verdade	
+		//E/And
+		System.out.println("E");
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(false && false);
+		System.out.println("\n");
+		
+		//Ou/Or
+		System.out.println("OU");
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+		System.out.println("\n");
+		
+		//OuExclusivo/XOR
+		System.out.println("Ou Exclusivo");
+		System.out.println(true ^ true);
+		System.out.println(true ^ false);
+		System.out.println(false ^ true);
+		System.out.println(false ^ false);
+	}
+
+}
