304d5ca9dc6e93a7176869b86ac19531fbd4cd0c
Daniel Santana <daniel10gja@gmail.com>
2021-10-14 01:20:34 -0300

    OpUnarios

---
+++ src/b_fundamentos/F_OpUnarios.java
@@ -0,0 +1,22 @@
+package b_fundamentos;
+
+public class F_OpUnarios {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		
+		a++; // a + 1
+		a--; // a - 1
+		
+		++b; // b + 1
+		--b; // b - 1
+		
+		System.out.println(++a == b--);
+		System.out.println(a == b);
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
