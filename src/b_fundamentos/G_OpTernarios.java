661e5c0dbd7f40278627f6dafdf6ca0fdcf1e934
Daniel Santana <daniel10gja@gmail.com>
2021-10-14 01:20:43 -0300

    OpTernarios

---
+++ src/b_fundamentos/G_OpTernarios.java
@@ -0,0 +1,14 @@
+package b_fundamentos;
+
+public class G_OpTernarios {
+	
+	public static void main(String[] args) {
+		
+		double media = 7.9;
+		String Result = media >= 7.0 ? "Aluno Aprovado" : "Aluno Reprovado";
+		
+		System.out.println(Result);
+			
+	}
+
+}
