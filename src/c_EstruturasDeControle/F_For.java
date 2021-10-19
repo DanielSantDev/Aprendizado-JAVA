412555a6993e5349f2849d9eb920358039d001aa
Daniel Santana <daniel10gja@gmail.com>
2021-10-19 13:11:10 -0300

    For

---
+++ src/c_EstruturasDeControle/F_For.java
@@ -0,0 +1,18 @@
+package c_EstruturasDeControle;
+
+public class F_For {
+
+	public static void main(String[] args) {
+		
+		for(int cont = 0 ; cont < 5 ; cont ++) {
+			System.out.println("Bom dia!");
+			System.out.printf("cont = %d\n", cont);
+		}
+		
+		/*for(;;) {
+			System.out.println("Laço Infinito");
+		}*/
+	
+	}
+
+}
