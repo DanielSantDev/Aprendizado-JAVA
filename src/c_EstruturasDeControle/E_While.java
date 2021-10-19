14962528fea86b6c25c85187aa5e93c35b975af7
Daniel Santana <daniel10gja@gmail.com>
2021-10-19 13:11:02 -0300

    While

---
+++ src/c_EstruturasDeControle/E_While.java
@@ -0,0 +1,16 @@
+package c_EstruturasDeControle;
+
+public class E_While {
+	
+	public static void main(String[] args) {
+		
+		int cont=0;
+		while(cont<10) {
+			System.out.println("Bom dia");
+			System.out.printf("contador = %d\n", cont);
+			cont++;
+		}
+		
+	}
+
+}
