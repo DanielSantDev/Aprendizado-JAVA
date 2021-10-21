4b5682f05b0f675cb756d7de4736cfc94b7f16d6
Daniel Santana <daniel10gja@gmail.com>
2021-10-21 11:05:37 -0300

    DoWhile

--- src/c_EstruturasDeControle/G_DoWhile.java
+++ src/c_EstruturasDeControle/G_DoWhile.java
@@ -1,5 +1,22 @@
 package c_EstruturasDeControle;
 
+import java.util.Scanner;
+
 public class G_DoWhile {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String texto = "";
+		
+		do {
+			System.out.println("Digite algo: ");
+			System.out.println("Quer sair? ");
+			texto = entrada.nextLine();
+			
+		}while(!texto.equals("sim"));
+		
+	}
 
 }
