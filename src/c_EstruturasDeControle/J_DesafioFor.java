90b1e13ae0769bda4e8f30a384e4a2b605b47b56
Daniel Santana <daniel10gja@gmail.com>
2021-10-29 09:32:12 -0300

    Desafio For

---
+++ src/c_EstruturasDeControle/J_DesafioFor.java
@@ -0,0 +1,13 @@
+package c_EstruturasDeControle;
+
+public class J_DesafioFor {
+	
+	public static void main(String[] args) {
+		
+		for(String v = "#" ; !v.equals("######") ; v += "#" ) {
+			System.out.println(v);
+		}
+		
+	}
+
+}
