ba2b3e683cab28403521aa227cc86ae7c7c4e13a
Daniel Santana <daniel10gja@gmail.com>
2022-03-03 21:33:30 -0300

    Create ValorNulo.java

---
+++ src/d_ClasseseMetodos/Metodo/ValorNulo.java
@@ -0,0 +1,17 @@
+package d_ClasseseMetodos.Metodo;
+
+import javax.xml.crypto.Data;
+
+public class ValorNulo {
+
+    public static void main(String[] args) {
+
+        String s1 = "";
+        System.out.println("!!!");
+
+        String s2 = null;
+        System.out.println(s2.concat("???"));
+
+    }
+
+}
