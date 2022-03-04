873a572e0c23ae0a6cdac97c3d3d663e520ce18c
Daniel Santana <daniel10gja@gmail.com>
2022-03-04 20:40:44 -0300

    Arrays

---
+++ src/e_ArraysECollections/A_Arrays.java
@@ -0,0 +1,39 @@
+package e_ArraysECollections;
+
+import java.util.Arrays;
+
+public class A_Arrays {
+
+    public static void main(String[] args) {
+
+        double[] notasAlunosA = new double[4];
+        System.out.println(Arrays.toString(notasAlunosA));
+
+        notasAlunosA[0] = 7.9;
+        notasAlunosA[1] = 8;
+        notasAlunosA[2] = 45;
+        notasAlunosA[3] = 7.9;
+
+        /*Formas de percorrer o array*/
+        System.out.println(Arrays.toString(notasAlunosA));
+
+        double total = 0;
+        for(int i=0; i <= 3; i++){
+            System.out.println(notasAlunosA[i]);
+            total += notasAlunosA[i];
+        }
+        System.out.println("\n");
+        for(int i=0; i < notasAlunosA.length; i++){
+            System.out.println(notasAlunosA[i]);
+            total += notasAlunosA[i];
+        }
+
+        System.out.println("\n" + total / notasAlunosA.length);
+
+
+        final double notaArmazenada = 0;
+        double[] notasAlunosB = {6.9, 8.9, notaArmazenada, 10};
+
+    }
+
+}
