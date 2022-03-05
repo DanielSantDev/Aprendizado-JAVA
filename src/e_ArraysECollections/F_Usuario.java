89bd33cb52e93992dd3685211d37218a7b2ec78e
Daniel Santana <daniel10gja@gmail.com>
2022-03-05 00:22:33 -0300

    updates

---
+++ src/e_ArraysECollections/F_Usuario.java
@@ -0,0 +1,29 @@
+package e_ArraysECollections;
+
+import java.util.Objects;
+
+public class F_Usuario {
+
+    String nome;
+    String email;
+
+    public boolean equals(Object obj) {
+        if(obj instanceof F_Usuario){
+            F_Usuario outro = (F_Usuario) obj;
+
+            boolean nomeIgual = outro.nome.equals(this.nome);
+            boolean emailIgual = outro.email.equals(this.email);
+
+            return nomeIgual && emailIgual;
+        }else{
+            return false;
+        }
+
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+}
